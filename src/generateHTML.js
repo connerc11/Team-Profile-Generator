ca729fead7cb4a73dcbc7f9693d834d35eaca7ca
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-08-24 17:55:20 -0400

    updated cards for the project

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -3,13 +3,13 @@ function buildManagerCard(manager){
     <div class="col-4">
     <div class="card h-100">
     <div class="card-header">
-    <h3>${manager.name}</h3>
+    <h3> ${manager.name}</h3>
     <h4> Profile Information! </h4>
     </div>
     <div class="card-body">
     <p class="id">ID- ${manager.Id}</p>
     <p class="email">Email- ${manager.email}</p>
-    <p class="officeID">Office Number- ${manager.officeNumber} </p>
+    <p class="officeID">Office Number- ${manager.officeNumber}</p>
     </div>
   </div>
 </div>`
@@ -18,7 +18,7 @@ function buildEngineerCard(engineer){
     return `<div class="col-4">
     <div class="card h-100">
     <div class="card-header">
-    <h3>${engineer.name}</h3>
+    <h3> ${engineer.name}</h3>
     <h4> Profile Information </h4>
     </div>
     <div class="card-body">
@@ -33,7 +33,7 @@ function buildInternCard(intern){
     return `<div class="col-4">
     <div class="card h-100">
     <div class="card-header">
-    <h3>${intern.name}</h3>
+    <h3> ${intern.name}</h3>
     <h4> Profile Information </h4>
     </div>
     <div class="card-body">
@@ -62,14 +62,29 @@ function generateHTML(team){
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Team Portfolio Generator</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     </head>
     <body>
+    <header> 
+    <nav class="navbar" id="navbar"> 
+    <span class="navbar-brand mb-0 h1 w-100 text-center" id="nav"> Team Profile Generator</span>
+    </nav>
+    </header>
+    <main>
+    <div class="container">
+    <div class="row justify-content-center"
+  
      ${buildTeam(team)}   
-     
-    </body>
+     </div>
+     </div>
+     </main>
 
-    <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous"> </script>
+    </body>
+ 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    
     </html>`
 }
 
